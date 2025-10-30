8c06662f891760be4142ab76e8de86dc278d64f8
Ian Gallardo <ian@iangallardo.com>
2025-10-29 20:22:06 -0400

    added blog image into post

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function (eleventyConfig) {
   const { DateTime } = require("luxon");
 
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  eleventyConfig.setTemplateFormats(["md", "njk"]);
+
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: 'utc' })
     .toFormat("EEEE, LLLL d',' yyyy");
